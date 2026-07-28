6cc0ba309510e27460725d2fbc4018a7d657731d
GmacSpm <giovanemacedo25@gmail.com>
2026-07-27 23:38:25 -0300

    Added proxy option

--- config/youtubeConfig.js
+++ config/youtubeConfig.js
@@ -2,5 +2,6 @@ export const defaultYtOptions = {
 	format: 'bestvideo[height<=360][ext=mp4]+bestaudio[ext=m4a]/best[height<=360][ext=mp4]/best',
 	mergeOutputFormat: 'mp4',
 	noCheckCertificates: true,
-	noWarnings: true, 
+	noWarnings: true,
+	proxy: '',
 };
